6b81be39fcd5c377d4c7132071153656c16d8d5b
김승은 <tmddms0317@naver.com>
2023-04-22 22:14:43 +0900

    style : 주석추가 - 프로젝트 참고 URL

--- src/main/java/com/example/todo/ToDoList/ToDoListApplication.java
+++ src/main/java/com/example/todo/ToDoList/ToDoListApplication.java
@@ -3,6 +3,8 @@ package com.example.todo.ToDoList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring으로 TO-DO List 만들기 실습
+// https://velog.io/@suzu11/spring-boot-TO-DO-List-%EB%A7%8C%EB%93%A4%EA%B8%B0-1
 @SpringBootApplication
 public class ToDoListApplication {
 
